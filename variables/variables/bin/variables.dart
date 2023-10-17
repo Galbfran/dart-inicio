5667fa98631d3aebe40a338b66e2cc2a0229ff52
Galbfran <francogalbiati984@gmail.com>
2023-10-17 18:58:42 -0300

    subo metodos string

--- variables/variables/bin/variables.dart
+++ variables/variables/bin/variables.dart
@@ -17,4 +17,24 @@ void main(List<String> arguments) {
   //puede cambiar su tipo
   dynamic variableDinamica = "hola";
   variableDinamica = variableEntera;
+
+  //metodos string
+  String correo;
+  String mensaje;
+
+  correo = "franco@gmail.com";
+  //retorna true si la cadena tiene el valor que se ingresa
+  print(correo.contains("@"));
+  //retorna true si la cadema finaliza en  el valor
+  print(correo.endsWith(".com"));
+
+  mensaje = correo.contains("@")
+      ? "Es un correo electronico "
+      : "No es un correo electronico";
+
+  print(mensaje);
+
+  //substring trae los valores entre las posiciones que le pedimos
+  print("longitud de correo: ${correo.length}");
+  print(correo.substring(2, 8));
 }
