21868274dff28c0421c80ed18cdb9dc0521e9720
Galbfran <francogalbiati984@gmail.com>
2023-10-17 18:10:48 -0300

    primeros pasos

--- hola_mundo/bin/hola_mundo.dart
+++ hola_mundo/bin/hola_mundo.dart
@@ -1,5 +1,38 @@
 import 'package:hola_mundo/hola_mundo.dart' as hola_mundo;
 
 void main(List<String> arguments) {
-  print('Hello world: ${hola_mundo.calculate()}!');
+  print('Hello world, primeros pasos en dart ${hola_mundo.calculate()}!');
+  var name = "franco galbiati";
+  var edad = 31;
+  var diametro = 3.7;
+  var array = [1, 2, 3];
+  var objeto = {
+    "nombre": "franco",
+  };
+
+  print(objeto["nombre"]);
+
+  if (edad >= 18) {
+    print("es mayor de edad");
+  } else {
+    print("es menor de edad");
+  }
+
+  for (int num in array) {
+    print(num);
+  }
+
+  var array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  for (int i = 0; i < array2.length; i++) {
+    print(array2[i]);
+  }
+
+  int fibonacci(int n) {
+    if (n == 0 || n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  var resultado = fibonacci(10);
+  print(resultado);
 }
