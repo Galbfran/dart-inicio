b117735cfee729a3a2a44436fbc414102fc6a4bb
Galbfran <francogalbiati984@gmail.com>
2023-10-22 20:59:35 -0300

    subo segundo ejercicio mensajes

--- flutter/fluter_tres/flutter_application_2/lib/main.dart
+++ flutter/fluter_tres/flutter_application_2/lib/main.dart
@@ -7,6 +7,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -14,12 +16,44 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter layout demo'),
         ),
         body: Column(
-          children: [titleSection],
+          children: [titleSection, textSection],
         ),
       ),
     );
   }
 }
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: color,
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Color color = Theme.of(context).primaryColor;
+
+Widget buttonSection = Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    _buildButtonColumn(color, Icons.call, 'CALL'),
+    _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+    _buildButtonColumn(color, Icons.share, 'SHARE'),
+  ],
+);
 
 Widget titleSection = Container(
   padding: const EdgeInsets.all(32),
@@ -56,5 +90,18 @@ Widget titleSection = Container(
       ),
       const Text('41'),
     ],
+  ),
+);
+
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: const Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
   ),
 );
